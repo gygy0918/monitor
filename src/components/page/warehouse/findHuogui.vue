1bbb8486b6e1c6cc23dca438738f5663175fe39b
guoying <guoying@corp.netease.com>
2018-07-18 20:42:11 +0800

    货柜阴影

--- src/components/page/warehouse/findHuogui.vue
+++ src/components/page/warehouse/findHuogui.vue
@@ -1,11 +1,11 @@
 <template>
     <el-row>
-        <el-col :span="4" v-for="(o, index) in data" :key="o" :offset="index > 0 ? 0 : 0">
+        <el-col :span="3" v-for="(o, index) in data" :key="o" :offset="index > 0 ? 0 : 0">
             <el-card :body-style="{ padding: '0px'}" class="csrd">
-                <img src="../../../assets/0.jpg" class="image">
+                <img src="../../../assets/0.jpg" class="image" >
                 <div style="padding: 14px;">
                     <span>{{o}}</span>
-                    <div class="bottom clearfix">
+                    <div >
                         <time class="time">{{ currentDate }}</time>
                         <el-button type="text" class="button">打开货柜</el-button>
                     </div>
@@ -47,6 +47,7 @@
         width: 100%;
         height: 100px;
         display: block;
+        /*box-shadow:3px 3px 8px #f00*/
     }
 
     .clearfix:before,
@@ -59,8 +60,12 @@
         clear: both
     }
     .csrd{
-        width: 150px;
-        height: 170px;
+        width: 120px;
+        height: 150px;
         margin-bottom: 20px;
+        box-shadow:3px 3px 8px #242f42;
     }
+    /*.liti{*/
+        /*box-shadow:-2px 0px 8px #f00,0px -2px 8px #000,0px 2px 8px #9e038a,2px 0px 8px #0f0;*/
+    /*}*/
 </style>
